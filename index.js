6b162ea796c02c388c67cbaebb6e3d2cb403c5d8
Favour Achara <acharafavour01@example.com>
2024-05-08 23:04:03 +0100

    updates

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const adviceId=document.querySelector('.advice_id')
 const advice=document.querySelector('.advice')
 const dice=document.querySelector('.circle')
+const leftDoubleQuote = '\u201C'; // “
+const rightDoubleQuote = '\u201D'; // ”
 
 const getAdvice=()=>{
     function processData(data) {
         // Process the data received from the API
         console.log(data.slip)
         adviceId.textContent=`#${data.slip.id}`
-        advice.textContent=`${data.slip.advice}`
+        advice.textContent=`${leftDoubleQuote}${data.slip.advice}${rightDoubleQuote}`
     }
 
 // Send the GET request
